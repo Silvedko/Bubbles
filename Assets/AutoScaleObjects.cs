61ef24e630f1e27a5cc219e5504da3e0da3981ba
Silvedko <silvedko@gmail.com>
2014-09-18 14:14:20 +0400

    [Bubbles] Add autocenter to circle collider.

---
+++ Assets/AutoScaleObjects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScaleObjects : MonoBehaviour {
+
+	public Vector2 scaleRatio = new Vector2 (0.1f, 0.1f);
+	private Transform myTrans;
+	private float widthHeightRatio;
+
+	void Start () 
+	{
+		myTrans = transform;
+		widthHeightRatio = (float) Screen.width / Screen.height;
+		SetScale ();
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void SetScale ()
+	{
+		myTrans.localScale = new Vector3 (scaleRatio.x, widthHeightRatio * scaleRatio.y, 1f);
+	}
+}
