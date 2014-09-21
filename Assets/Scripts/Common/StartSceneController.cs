0c9aef25dd6c644673187678f3e17f952d719046
Silvedko <silvedko@gmail.com>
2014-09-22 00:03:23 +0400

    [Bubbles] Add StartScene with controller

---
+++ Assets/Scripts/Common/StartSceneController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneController : MonoBehaviour 
+{
+	void Start () 
+	{
+		GameManager.Instance.linkCounter = 0;
+		Debug.Log (GameConstants.bubbleLinks.Length);
+		for (int i = 0; i < GameConstants.bubbleLinks.Length; i++) 
+		{
+			GameObject gOLoader = new GameObject("ImageLoader " + i);
+			var iLoader = gOLoader.AddComponent <ImageDownloader>();
+			iLoader.linkCounter = GameManager.Instance.linkCounter;
+			GameManager.Instance.linkCounter ++;
+		}
+	}
+	
+	void OnGUI () 
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Start Game")) 
+		{
+			Application.LoadLevel(GameConstants.gameSceneName);
+		}
+	}
+}
