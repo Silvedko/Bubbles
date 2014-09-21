daac58179c52076922b8b5a8424d6eb45d7fbe52
Silvedko <silvedko@gmail.com>
2014-09-21 17:57:54 +0400

    [Bubbles] Add materials, change scene

---
+++ Assets/Scripts/Common/ObjectStateHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectStateHandler : MonoBehaviour
+{
+	void Start ()
+	{
+		NotificationCenter.DefaultCenter.AddObserver (this, GameConstants.bubbleBlowNotificationMessage);
+		NotificationCenter.DefaultCenter.AddObserver (this, GameConstants.bubbleDeniedNotificationMessage);
+	}
+
+	void BubbleBlow(NotificationCenter.Notification sender)
+	{
+//		Destroy(sender);
+	}
+}
