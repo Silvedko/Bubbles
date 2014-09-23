fef8b16ea7a38817d1bf69df723c1cb3294c01b3
Silvedko <silvedko@gmail.com>
2014-09-24 00:03:41 +0400

    [Bubbles] Finishing resizable touches, and add blocking button when textures can't load from www

--- Assets/Scripts/Common/StartSceneController.cs
+++ Assets/Scripts/Common/StartSceneController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class StartSceneController : MonoBehaviour 
 {
 	NotificationCenter ntfCenter = null;
+	int notificationCounter = 0;
 
 	void Start () 
 	{
@@ -22,14 +23,26 @@ public class StartSceneController : MonoBehaviour
 	
 	void OnGUI () 
 	{
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Start Game")) 
+		if (notificationCounter == 4) 
 		{
-			Application.LoadLevel(GameConstants.gameSceneName);
+			if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Start Game")) {
+					Application.LoadLevel (GameConstants.gameSceneName);
+			}
 		}
+		else
+		{
+			GUIStyle myStyle = new GUIStyle();
+			myStyle.normal.textColor = Color.cyan;
+			myStyle.fontSize = 35;
+
+			int tempLoadingValue = 4 - notificationCounter;
+			GUI.Label((new Rect (Screen.width / 2 , Screen.height / 2, 200, 100)), tempLoadingValue.ToString() + "...", myStyle);
+	    }
 	}
 
 	void OnLoadComplete (NotificationCenter.Notification notification)
 	{
+		notificationCounter++;
 		Destroy (notification.goSender);
 	}
 
