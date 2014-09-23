f9b20c2432e0bf2ec1fd263f9b759d0ddfbfc7ea
Silvedko <silvedko@gmail.com>
2014-09-23 18:18:11 +0400

    [Bubbles] Add score script

---
+++ Assets/Scripts/Common/ScoreGUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreGUI : MonoBehaviour 
+{
+	int scoreWidth = 100;
+	int scoreHeight = 50;
+	
+	void OnGUI () 
+	{
+		GUIStyle myStyle = new GUIStyle();
+		myStyle.normal.textColor = Color.cyan;
+		myStyle.fontSize = 35;
+
+		GUI.Label (new Rect(0f, Screen.height - scoreHeight, scoreWidth, scoreHeight), GameManager.Instance.gamePoints.ToString(), myStyle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
