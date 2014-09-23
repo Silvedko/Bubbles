e7c0e30fdc3a535e74ae0e65cd61e81e831348ec
Silvedko <silvedko@gmail.com>
2014-09-23 18:30:50 +0400

    [Bubbles] Add comments

--- Assets/Scripts/Generator/TextureGenerator.cs
+++ Assets/Scripts/Generator/TextureGenerator.cs
@@ -1,43 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class TextureGenerator : MonoBehaviour {
+public class TextureGenerator : MonoBehaviour 
+{
+	public BubbleController bblCntroller;
 
-	public Sprite gOSprite;
+	private Sprite gOSprite; 
 	private Texture2D generatedTexture;
-	int textureWidth;
+	private int textureWidth;
+	private List <Color> colors = null;
 
-	void Start () 
+	NotificationCenter ntfCenter;
+
+	void Awake ()
 	{
-		gOSprite = gameObject.GetComponent<SpriteRenderer> ().sprite;
-		textureWidth = gOSprite.texture.width;
-
-		GenerateTexture (gOSprite.texture.width);
-
-//		StartThreads ();
-
-		gOSprite = Sprite.Create (generatedTexture, new Rect (0f, 0f, 32f, 32f), Vector2.zero);
-
-		gameObject.GetComponent<SpriteRenderer> ().sprite = gOSprite;
-		renderer.material.mainTexture = gOSprite.texture;
-		gameObject.GetComponent<SpriteRenderer> ().material.SetTexture ("CullingMask", GameManager.Instance.textures [0]);
-
+		ntfCenter = NotificationCenter.DefaultCenter;
+		ntfCenter.AddObserver (this, GameConstants.onBubbleCreated);
 	}
 
-//	private void StartThreads()
-//	{
-//		for (int i = 0; i < 4; i++)
-//		{
-//			System.Threading.ThreadStart pts = new System.Threading.ThreadStart(ThreadedColorCopy);
-//			System.Threading.Thread workerForOneRow = new System.Threading.Thread(pts);
-//			workerForOneRow.Start(i);
-//		}
-//	}
-//
-//	private void ThreadedColorCopy()
-//	{
-//		GenerateTexture (textureWidth);
-//	}
+	void OnBubbleCreated () 
+	{
+		Init ();
+	}
+
+	void OnDisable ()
+	{
+		Destroy (generatedTexture);
+		Destroy (gOSprite);
+		colors.Clear();
+		colors.TrimExcess ();
+	}
+
+	void OnDestroy ()
+	{
+		if(ntfCenter != null && !ntfCenter.Equals (null))
+			ntfCenter.RemoveObserver (this, GameConstants.onBubbleCreated);
+	}
+
+	void Init ()
+	{
+		colors = new List<Color> ();
+		textureWidth = (int) bblCntroller.textureWidth;
+		GenerateTexture (textureWidth);
+		
+		gOSprite = Sprite.Create (generatedTexture, new Rect (0.0f, 0.0f, textureWidth, textureWidth), new Vector2(0.5f, 0.5f));
+		
+		gameObject.GetComponent<SpriteRenderer> ().sprite = gOSprite;
+		renderer.material.mainTexture = generatedTexture;
+		gameObject.GetComponent<SpriteRenderer> ().material.SetFloat ("_Cutoff", 0.5f);
+		if(gameObject.GetComponent<SpriteRenderer> ().material.HasProperty("_Mask"))
+			gameObject.GetComponent<SpriteRenderer> ().material.SetTexture ("_Mask", (Texture) GetMaskFromSize (textureWidth));
+	}
+
+	private Texture2D GetMaskFromSize (int size)
+	{
+		Texture2D tempTexture = null;
+		foreach (var neededTexture in GameManager.Instance.textures) 
+		{
+			if (neededTexture.width == size)
+				tempTexture = (Texture2D) neededTexture;
+		}
+		return tempTexture;
+	}
+	
 
 	private void GenerateTexture (int textureWidth)
 	{
@@ -50,21 +76,26 @@ public class TextureGenerator : MonoBehaviour {
 		
 	private Texture2D MakeTopTexture (Texture2D texture)
 	{
-		for (int i = 0; i < texture.width; i++)
-			for (int j = 0; j < texture.height; j++) 
-			{
-				texture.SetPixel (i, j, GetRandomColor());
-			}
-			texture.Apply();
+		int randColR = Random.Range (1, 255);
+		int randColG = Random.Range (1, 255);
+		int randColB = Random.Range (1, 255);
+
+		for (int i = 0; i < texture.width * texture.height; i++) 
+		{
+			colors.Add (GetRandomColor (i, randColR, randColG, randColB));
+		}
+			
+		texture.SetPixels (colors.ToArray());
+		texture.Apply();
 
 		return texture;
 	}
 
-	private Color GetRandomColor ()
+	private Color GetRandomColor (int counter, int randColorR, int randColorG, int randColorB)
 	{
-		float r = (Random.Range (0f, 255f)) / 255f;
-		float g = (Random.Range (0f, 255f)) / 255f;
-		float b = (Random.Range (0f, 255f)) / 255f;
+		float r = ((randColorR + counter) % 255) / 255f;
+		float g = ((randColorG + counter) % 255) / 255f;;
+		float b = ((randColorB + counter) % 255) / 255f;;
 
 		return new Color (r, g, b, 1.0f);
 	}
